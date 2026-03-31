b0ea7693083b4628b3deefb9940d55e203fbcd27
S A Faroque <faroque.computer@gmail.com>
2026-03-31 14:48:24 +0600

    first commit

---
+++ faroque-website-background.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin name: Faroque Website Background
+ * Plugin URI: https://github.com/safaroque7/faroque-website-background
+ * Description: It is a plugin where you can change background color of your website.
+ * Version: 1.0.0
+ * Author: S A Faroque
+ * Author URI: https://webnewsdesign.com
+ * Text Domain: faroque-website-background
+ */
+
+//Exit if accessed deirectly
+if (!defined('ABSPATH')) exit;
+
+//Include Functions 
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+
+//Enqueue CSS & JS
+function fwb_enqueue_assets()
+{
+    wp_enqueue_style('fwb_style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
+    wp_enqueue_script('fwb_script', plugin_dir_url(__FILE__) . 'assets/js/script.js');
+}
+add_action('wp_enqueue_scripts', 'fwb_enqueue_assets');
